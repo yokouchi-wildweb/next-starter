a5e4ae55b5285bd7a916a2f31a1ca5ae309bf390
Yokouchi Yusuke <yokouchi@wildweb.tokyo>
2025-11-10 12:24:40 +0900

    m

--- src/components/Layout/Flex.tsx
+++ src/components/Layout/Flex.tsx
@@ -1,6 +1,7 @@
 
 import { cva, type VariantProps } from "class-variance-authority";
-import type { ComponentPropsWithoutRef } from "react";
+import {ComponentPropsWithoutRef} from "react";
+import type { JSX } from "react";
 
 import { cn } from "@/lib/cn";
 
