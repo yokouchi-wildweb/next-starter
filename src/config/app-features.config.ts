c2cd51e9cd28b804b558caae76bc9bdc3cf1f5a3
Yokouchi Yusuke <yokouchi@wildweb.tokyo>
2025-11-10 19:24:57 +0900

    m

--- src/config/app-features.config.ts
+++ src/config/app-features.config.ts
@@ -21,8 +21,8 @@ export const APP_FEATURES = {
     },
     dashboard: {
       sections: {
-        showMainMetrics: true,
-        showAdditionalMetrics: true,
+        showMainMetrics: false,
+        showAdditionalMetrics: false,
       },
     },
   },
