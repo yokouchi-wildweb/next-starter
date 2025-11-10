2877d8a4a70423802f38f0590e55e8038e8650f7
Yokouchi Yusuke <yokouchi@wildweb.tokyo>
2025-11-10 19:36:54 +0900

    m

--- src/types/adminMenu.ts
+++ src/types/adminMenu.ts
@@ -4,13 +4,13 @@ import type { ReactNode } from "react";
 
 export type AdminMenuItem = {
   title: string;
-  href: string;
+  href: string | null;
   icon?: ReactNode;
 };
 
 export type AdminMenuSection = {
   title: string;
-  href?: string;
+  href?: string | null;;
   items: AdminMenuItem[];
 };
 
