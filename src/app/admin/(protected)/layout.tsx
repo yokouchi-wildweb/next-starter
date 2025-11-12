9ac110b8f354c946f92e25aea778ebc4c4c84706
Yokouchi Yusuke <yokouchi@wildweb.tokyo>
2025-11-12 09:01:33 +0900

    m

--- src/app/admin/(protected)/layout.tsx
+++ src/app/admin/(protected)/layout.tsx
@@ -13,17 +13,16 @@ import type { UserRoleType } from "@/types/user";
 export default async function AdminLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
+
   const ROLE_ADMIN: UserRoleType = "admin";
   const { total: adminUserCount } = await userService.search({
     limit: 1,
     where: { field: "role", op: "eq", value: ROLE_ADMIN },
   });
 
-  if (adminUserCount === 0) {
-    // 管理者ユーザーが未作成の環境では、保護された管理画面全体へのアクセスを初期セットアップ導線へ統一する
-    // ため、初期化フローを開始する専用ページへ即座にリダイレクトする
-    redirect("/admin/setup");
-  }
+  // 管理者ユーザーが未作成の環境では、保護された管理画面全体へのアクセスを初期セットアップ導線へ統一する
+  // ため、初期化フローを開始する専用ページへ即座にリダイレクトする
+  if (adminUserCount === 0) redirect("/admin/setup");
 
   await authGuard({ allowRoles: ["admin"], redirectTo: "/admin/login" });
   const setting = await settingService.getGlobalSetting();
