dd35cc0f28cfdc3056eff3b9f131b2246a096f29
Yokouchi <yokouchi@wildweb.tokyo>
2026-01-18 17:04:32 +0900

    ドキュメントをセーブ

--- src/components/Form/Controlled/index.ts
+++
@@ -1,4 +0,0 @@
-// src/components/Form/Controlled/index.ts
-// Input/Controlled からの再エクスポート
-
-export * from "../Input/Controlled";
--- src/components/Form/Manual/index.ts
+++
@@ -1,4 +0,0 @@
-// src/components/Form/Manual/index.ts
-// Input/Manual からの再エクスポート
-
-export * from "../Input/Manual";
