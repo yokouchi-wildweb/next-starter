2f619de44f2da2bf81b1da05b23a00869b24454d
Yokouchi <yokouchi@wildweb.tokyo>
2026-01-24 22:27:10 +0900

    m

--- src/registry/profileTableRegistry.ts
+++ src/registry/profileTableRegistry.ts
@@ -5,5 +5,5 @@
 // アンカーコメント間のコードは自動生成スクリプトによって更新されます。
 
 // === AUTO-GENERATED EXPORTS START ===
-export * from "@/features/core/userProfile/generated/contributor";
+export * from "@/features/core/userProfile/generated/contributor/drizzle";
 // === AUTO-GENERATED EXPORTS END ===
