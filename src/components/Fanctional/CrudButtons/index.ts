231e5080398bbf41a84c43ff602e2e717c3d85c2
Yokouchi <yokouchi@wildweb.tokyo>
2026-01-24 21:40:02 +0900

    不要になったコンポーネントを削除

--- src/components/Fanctional/CrudButtons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as BulkDeleteButton } from "./BulkDeleteButton";
-export { default as CreateButton } from "./CreateButton";
-export { default as DeleteButton } from "./DeleteButton";
-export { default as DuplicateButton } from "./DuplicateButton";
-export { default as EditButton } from "./EditButton";
-export { default as HardDeleteButton } from "./HardDeleteButton";
