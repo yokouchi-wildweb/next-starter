e14bd5b777561f2e4f2dca715b3c49d8cae5c240
Yokouchi <yokouchi@wildweb.tokyo>
2026-01-27 14:52:59 +0900

    m

--- src/components/Overlays/ImageViewer/ZoomableImage.tsx
+++ src/components/Overlays/ImageViewer/ZoomableImage.tsx
@@ -3,10 +3,12 @@
 "use client";
 
 import { type ComponentPropsWithoutRef, type MouseEvent } from "react";
-import { useImageViewer } from "./context";
+import { useImageViewer, type ImageViewerOptions } from "./context";
 
 type ZoomableImageProps = ComponentPropsWithoutRef<"img"> & {
   src: string;
+  /** 拡大表示時のサイズオプション */
+  viewerOptions?: ImageViewerOptions;
 };
 
 export function ZoomableImage({
@@ -14,13 +16,14 @@ export function ZoomableImage({
   alt = "",
   style,
   onClick,
+  viewerOptions,
   ...rest
 }: ZoomableImageProps) {
   const { openImage } = useImageViewer();
 
   const handleClick = (e: MouseEvent<HTMLImageElement>) => {
     e.stopPropagation();
-    openImage(src, alt);
+    openImage(src, alt, viewerOptions);
     onClick?.(e);
   };
 
