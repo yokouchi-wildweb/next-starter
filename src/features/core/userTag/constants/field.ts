bdae60877b7aedc39b983fc45ffb10f60f7ffc2d
Yokouchi <yokouchi@wildweb.tokyo>
2026-02-11 17:13:59 +0900

    m

--- src/features/core/userTag/constants/field.ts
+++ src/features/core/userTag/constants/field.ts
@@ -4,12 +4,12 @@
 // 手動での編集は変更が上書きされる可能性があるため推奨されません。
 
 export const UserTagColorOptions = [
-  { value: "primary", label: "プライマリー" },
-  { value: "secondary", label: "セカンダリー" },
+  { value: "primary", label: "プライマリ" },
+  { value: "secondary", label: "セカンダリ" },
   { value: "accent", label: "アクセント" },
-  { value: "success", label: "サクセス" },
-  { value: "warning", label: "ワーニング" },
-  { value: "destructive", label: "ディストラクティブ" },
+  { value: "success", label: "成功色" },
+  { value: "warning", label: "注意色" },
+  { value: "destructive", label: "警告色" },
   { value: "red", label: "レッド" },
   { value: "orange", label: "オレンジ" },
   { value: "yellow", label: "イエロー" },
