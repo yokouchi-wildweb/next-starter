e55ff9b5e7878e3a3ac1fd121a60d5dcd6ffad4e
Yokouchi <yokouchi@wildweb.tokyo>
2026-02-11 20:01:55 +0900

    ログインページのリダイレクト関連の問題を修正

--- src/app/(user)/(auth)/login/page.tsx
+++ src/app/(user)/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 // src/app/(user)/(auth)/login/page.tsx
 
+import { redirect } from "next/navigation";
+
 import { UserPageTitle } from "@/components/AppFrames/User/Elements/PageTitle";
 import { UserPage } from "@/components/AppFrames/User/Layout/UserPage";
 import { Flex } from "@/components/Layout/Flex";
 import { UserLogin } from "@/features/core/auth/components/UserLogin";
+import { authGuard } from "@/features/core/auth/services/server/authorization";
 
 type UserLoginPageProps = {
   searchParams?: Promise<{
@@ -13,6 +16,12 @@ type UserLoginPageProps = {
 
 export default async function UserLoginPage({ searchParams }: UserLoginPageProps) {
   const returnTo = searchParams ? (await searchParams).returnTo : undefined;
+
+  // すでにログイン済みの場合はリダイレクト
+  const session = await authGuard();
+  if (session) {
+    redirect(returnTo || "/");
+  }
 
   return (
     <UserPage containerType="narrowStack">
