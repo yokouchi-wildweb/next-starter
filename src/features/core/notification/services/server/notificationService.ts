e370761378122530b15dc4f73e4993082e982fda
Yokouchi <yokouchi@wildweb.tokyo>
2026-03-08 16:27:14 +0900

    お知らせ通知の機能を実装中。フェーズ2が完了。

--- src/features/core/notification/services/server/notificationService.ts
+++ src/features/core/notification/services/server/notificationService.ts
@@ -1,7 +1,11 @@
 // src/features/notification/services/server/notificationService.ts
 
 import { base } from "./drizzleBase";
+import { create } from "./wrappers/create";
+import { update } from "./wrappers/update";
 
 export const notificationService = {
   ...base,
+  create,
+  update,
 };
